80dd18bdf5e697224aa7da096a09a606e1afd473
Matus Luptak <113360574+Shuterix@users.noreply.github.com>
2024-10-23 10:32:26 +0200

    [Auth] register complete

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    public function register()
+	{
+		return view('auth.register');
+	}
+
+	public function store()
+	{
+		$validated = request()->validate([
+			'name' => 'required|min:3|max:40',
+			'email' => 'required|email|unique:users,email',
+			'password' => 'required|confirmed|min:8',
+		]);
+
+		User::create(
+			[
+				'name' => $validated['name'],
+				'email' => $validated['email'],
+				'password' => Hash::make($validated['password']),
+			]
+		);
+
+		return redirect()->route('dashboard')->with('success', 'Account created successfully');
+	}
+}
--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,21 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CommentController;
 
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+
+// POST
 Route::post('/post', [PostController::class, 'store'])->name('post.create');
 Route::delete('/post/{post}', [PostController::class, 'destroy'])->name('post.destroy');
 Route::get('/post/{post}', [PostController::class, 'show'])->name('post.show');
 Route::get('/post/{post}/edit', [PostController::class, 'edit'])->name('post.edit');
 Route::put('/post/{post}', [PostController::class, 'update'])->name('post.update');
 Route::post('/posts/{post}/comments', [CommentController::class, 'store'])->name('comments.store');
+
+// AUTH
+Route::get('/register', [AuthController::class, 'register'])->name('auth.register');
+Route::post('/register', [AuthController::class, 'store'])->name('auth.store');
